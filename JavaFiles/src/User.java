fa0c234d550e6fd30861aee4669339ef6be01e6d
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2018-04-11 10:03:04 -0400

    Update User.java

--- JavaFiles/src/User.java
+++ JavaFiles/src/User.java
@@ -12,7 +12,9 @@ public class User {
 	public String city;
 	public String state;
 	public int zip;
-	l= "'"
+	l= "'" //Use these to make life easier while formatting
+	L = "',"
+		
 	public void login(String emailAddr, String password) {
 		
 	}
@@ -21,17 +23,23 @@ public class User {
 		
 	}
 	
-	public void changeInfo(Statement s, String email, String field, String info) {
-	s.executeUpdate("Update Users "+ 
+	public void changeInfo(Statement s, String email, String field, String info,int n ) {
+	if n = 0 {
+		s.executeUpdate("Update Users "+ 
 			"SET "+field+"="+l+info+l + 
 			"WHERE email="  +l+email+l)  
+	}
+	else {
+		s.executeUpdate("Update Users "+ 
+			"SET "+field+"="+l+int(info)+l + 
+			"WHERE email="  +l+email+l)  
+	}
 	}
 	
 	public void changePassword(Statement s, String passwd,String emailAddr,) {
 		s.executeUpdate("Update Users 
 			SET + "='"+passwd+"'" + 
-			"WHERE emailAddr="'"+emailAddr+"'")
-		
+			"WHERE emailAddr="'"+emailAddr+"'")								
 	}
 	
 	public void changeEmail(Statement s, String emailAddr) {
@@ -44,8 +52,8 @@ public class User {
 	public void savePayment(Statement s, String email,  int cardNum, String cardHolderName, String expDate) {
 	try {
 	s.executeUpdate("insert into payment_info" + 
-			" values('" +email_id+"','"+card_name +"','"+card_number +"','"+cardholder_name 
-			+"','"+exp_date+"','"+ street+"','"+city+"','"+state+"','"+zip+"');" );
+			" values(" l+email_id+L+card_name +L+card_number +L+cardholder_name 
+			+L+exp_date+L+ street+L+city+L+state+L+zip+"');" );
 }
 	catch (SQLException e) {
 		System.out.println("savePayment "+ e);
@@ -53,7 +61,14 @@ public class User {
 	
 	} 
 	
-	public void viewPurchase() {
+	public void viewPurchase(Statement s, String email) {
+		ResultSet r = s.executeQuery("Select t.ticket_id, m.movie_name, t.movie_id,t.date, t.time "+
+					     "FROM ticket as t, registered_tickets as rt,movie as m "+
+					     "WHERE rt.user_email=" l+email+l)
+		while (r.next()) {
+		System.out.print (r.getInt(1), r.getString(2),r.getDate(3), r.getTime(4) );
+		}
+		
 	}
 
 }
