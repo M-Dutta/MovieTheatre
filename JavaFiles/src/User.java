3b70cdd44f045de3101671433431f2d0ca8bee00
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2018-04-11 09:34:38 -0400

    Update User.java

--- JavaFiles/src/User.java
+++ JavaFiles/src/User.java
@@ -12,7 +12,7 @@ public class User {
 	public String city;
 	public String state;
 	public int zip;
-
+	l= "'"
 	public void login(String emailAddr, String password) {
 		
 	}
@@ -21,21 +21,36 @@ public class User {
 		
 	}
 	
-	public void changeInfo() {
+	public void changeInfo(Statement s, String email, String field, String info) {
+	s.executeUpdate("Update Users "+ 
+			"SET "+field+"="+l+info+l + 
+			"WHERE email="  +l+email+l)  
+	}
+	
+	public void changePassword(Statement s, String passwd,String emailAddr,) {
+		s.executeUpdate("Update Users 
+			SET + "='"+passwd+"'" + 
+			"WHERE emailAddr="'"+emailAddr+"'")
 		
 	}
 	
-	public void changePassword(String emailAddr) {
+	public void changeEmail(Statement s, String emailAddr) {
+		
+		
 		
 	}
 	
-	public void changeEmail(String emailAddr) {
-		
+	//should we be saving the security codes? === NOPE. Will fix that in the DB
+	public void savePayment(Statement s, String email,  int cardNum, String cardHolderName, String expDate) {
+	try {
+	s.executeUpdate("insert into payment_info" + 
+			" values('" +email_id+"','"+card_name +"','"+card_number +"','"+cardholder_name 
+			+"','"+exp_date+"','"+ street+"','"+city+"','"+state+"','"+zip+"');" );
+}
+	catch (SQLException e) {
+		System.out.println("savePayment "+ e);
 	}
 	
-	//should we be saving the security codes?
-	public void savePayment(String cardType,  int cardNum, String cardHolderName, int secCode, String expDate) {
-		
 	} 
 	
 	public void viewPurchase() {
