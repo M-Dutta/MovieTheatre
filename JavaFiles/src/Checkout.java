313b8d6f409e0d6626eb9cd9ff5a47dc84a6860b
Swiftie4lyfe <jtang25@uga.edu>
2018-04-11 21:56:50 -0400

    implemented new classes

---
+++ JavaFiles/src/Checkout.java
@@ -0,0 +1,18 @@
+import java.sql.Statement;
+
+public class Checkout {
+	public double total;
+	public CreditCardInfo paymentInfo;
+	
+	public void validate(Statement s, CreditCardInfo info) {
+		
+	}
+	
+	public void confirmation(Statement s, String email) {
+		
+	}
+	
+	public void updateTables(Statement s) {
+		
+	}
+}
