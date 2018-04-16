de59ce7eb9032813cf002f94aa900c6f82d5f7b3
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2018-04-15 22:16:06 -0400

    Add files via upload

--- testserver/src/registerServlet.java
+++ testserver/src/registerServlet.java
@@ -15,48 +15,42 @@ public class registerServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	public static Date DateConverter (String date) throws ParseException {
-	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-	java.util.Date utilDate = formatter.parse(date);
-	java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-	return sqlDate;
-	}
 	
 @Override
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-               throws IOException, ServletException {
-      // Set the response message's MIME type
-      response.setContentType("text/html; charset=UTF-8");
-      // Allocate a output writer to write the response message into the network socket
-      //PrintWriter out = response.getWriter();
-      
-    	  String fName = request.getParameter("firstName");
-    	  String lName = request.getParameter("lastName");
-    	  String email = request.getParameter("email");
-    	  String Password = request.getParameter("Password");
-    	  String passConfirm = request.getParameter("passConfirm");
-    	  String[] checkbox = request.getParameterValues("checkbox");
+public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+	
+    // Set the response message's MIME type
+    response.setContentType("text/html; charset=UTF-8");
+    // Allocate a output writer to write the response message into the network socket
+    //PrintWriter out = response.getWriter();
     
-      System.out.println("Name " +fName);
-      System.out.println("Lname " +lName);
-      System.out.println("email " +email);
-      System.out.println("pass " +Password);
-      System.out.println("pass " +passConfirm);
-      System.out.println("cb " +checkbox);
-      
-      if (Password.equals(passConfirm)) {
-    	  Password = Utilities.hasher(Password);  
-      }
-     
+  	  String fName = request.getParameter("firstName");
+  	  String lName = request.getParameter("lastName");
+  	  String email = request.getParameter("email");
+  	  String Password = request.getParameter("Password");
+  	  String passConfirm = request.getParameter("passConfirm");
+  	  String[] checkbox = request.getParameterValues("checkbox");
+  
+    System.out.println("Name " +fName);
+    System.out.println("Lname " +lName);
+    System.out.println("email " +email);
+    System.out.println("pass " +Password);
+    System.out.println("pass " +passConfirm);
+    System.out.println("cb " +checkbox);
+    
+    if (Password.equals(passConfirm)) {
+  	  Password = Utilities.hasher(Password);  
+    }
+   
 	try {
-		User nU = new User( email, Password,  fName, lName, "phone",DateConverter("1990-01-01") ,0, 0, "Blank", "Blank","Blank",0000000);
+		User nU = new User( email, Password,  fName, lName, "phone",Utilities.DateConverter("1990-01-01") ,0, 0, "Blank", "Blank","Blank",0000000);
 		nU.register(Utilities.stmt);
-	} catch (ParseException e) {
+		} catch (ParseException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
+		}
 	}
-     
-      
-   	}
-}
+}	
+
+
