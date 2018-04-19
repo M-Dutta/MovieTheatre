35f29e93a834e5ab94353c9b98d9410684ed283e
aubbreyo <alo75330@uga.edu>
2018-04-18 23:06:14 -0400

    Add files via upload

---
+++ testserver/src/EditEmployeeServlet.java
@@ -0,0 +1,43 @@
+
+import java.io.IOException; 
+import java.text.ParseException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditEmployeeServlet extends HttpServlet {
+ 
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+@Override
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+	
+	response.setContentType("text/html; charset=UTF-8");
+	
+	if (request.getParameter("save") != null)
+	{
+
+	}
+	
+	if (request.getParameter("back") != null)
+	{
+		response.sendRedirect("adminEmployeeInfo.html");	
+	}
+	
+	if (request.getParameter("saveNew") != null)
+	{
+
+	}
+	
+	
+}
+}
