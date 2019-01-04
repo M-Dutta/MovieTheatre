78ed12fba4b5f1594a6a6e8e7629c381b93a430b
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2019-01-04 15:39:17 -0500

    Update driver.java

--- JavaFiles/src/driver.java
+++ JavaFiles/src/driver.java
@@ -28,17 +28,7 @@ public class driver extends Utilities {
 			System.out.println(e);
 		}
 		
-	     /*
-		User jeff = new User( "Mynameisfeff@taylorSwift.com","jesusholdthewheel", "Jeff",
-				 "TK","666-777", DateConverter("1996-03-03")  ,1, 0,"Bronx", "detroit", "themoon", 66666);	
-		jeff.register(stmt);
-		**/
-		User dummy = new User("1","", null, null, null, null, 0, 0, null, null, null, 0);
-		User u  = new User();
-		u.getUser( stmt, "Mynameisfeff@taylorSwift.com");
-		System.out.println(u.email);
-		u.login( stmt, "Mynameisfeff@taylorSwift.com", "jesusholdthewheel" ) ;
-		dummy.login( stmt, "Mynameisfeff@taylorSwift.com","jesusholdthewheel" ) ;
+	   
 }
 	
 	
